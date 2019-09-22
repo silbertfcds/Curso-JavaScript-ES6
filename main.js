b94b5a3d0f1271b5016c0a4741190d4bd26a4639
Silbert <silbert.fcds@hotmail.com>
2019-09-22 16:43:44 -0300

    Criando projeto-classes

---
+++ main.js
@@ -0,0 +1,30 @@
+class List {
+    constructor(){
+        this.data = [];
+    }
+
+    add(data){
+        this.data.push(data);
+        console.log(this.data);
+    }
+}
+
+
+class TodoList extends List {
+    constructor(){
+        super();
+        this.usuario = 'silbert';
+    }
+
+    mostraUsuario(){
+        console.log(this.usuario);
+    }
+}
+
+var MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function(){
+    MinhaLista.add('Novo todo');
+}
+
+MinhaLista.mostraUsuario();
