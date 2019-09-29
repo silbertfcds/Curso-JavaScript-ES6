0a6c953b7568896fc56ca028fe54c8b03708b7e6
Silbert <silbert.fcds@hotmail.com>
2019-09-29 10:34:37 -0300

    Template Literals

--- bundle.js
+++ bundle.js
@@ -1,56 +1,6 @@
 "use strict";
 
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
-
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
-
-function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
-
-// REST
-var usuario = {
-  nome: 'silbert',
-  idade: 33,
-  empresa: 'Eu'
-};
-
-var nome = usuario.nome,
-    resto = _objectWithoutProperties(usuario, ["nome"]);
-
-console.log(nome);
-console.log(resto);
-var arr = [1, 2, 3, 4];
-var a = arr[0],
-    b = arr[1],
-    c = arr.slice(2);
-console.log(arr);
-
-function soma() {
-  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
-    params[_key] = arguments[_key];
-  }
-
-  return params.reduce(function (total, next) {
-    return total + next;
-  });
-}
-
-console.log(soma(1, 2, 3, 4)); // SPREAD
-
-var arr1 = [1, 2, 3];
-var arr2 = [4, 5, 6];
-var arr3 = [].concat(arr1, arr2);
-console.log(arr3);
-var usuario1 = {
-  nome: 'eu',
-  idade: 12
-};
-
-var usuario2 = _objectSpread({}, usuario1, {
-  nome: 'joão'
-});
-
-console.log(usuario2);
+var nome = "silbert";
+var idade = 33;
+console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos');
+console.log("Meu nome \xE9 ".concat(nome, " e tenho ").concat(idade, " anos."));
--- main.js
+++ main.js
@@ -1,42 +1,5 @@
-// REST
+const nome = "silbert";
+const idade = 33;
 
-const usuario = {
-    nome:'silbert',
-    idade: 33,
-    empresa: 'Eu'
-};
-
-const {nome, ...resto} = usuario;
-
-console.log(nome);
-console.log(resto);
-
-
-const arr = [1,2,3,4];
-
-const [a,b,...c] = arr;
-
-console.log(arr);
-
-function soma (...params){
-    return params.reduce((total, next)=> total + next);
-}
-
-console.log(soma(1,2,3,4));
-
-// SPREAD
-
-const arr1 = [1,2,3];
-const arr2 = [4,5,6];
-
-const arr3 = [...arr1, ...arr2];
-
-console.log(arr3);
-
-const usuario1 = {
-    nome: 'eu',
-    idade: 12,
-};
-
-const usuario2 = {...usuario1, nome: 'joão'};
-console.log(usuario2);
+console.log ('Meu nome é '+nome + ' e tenho '+ idade + ' anos');
+console.log(`Meu nome é ${nome} e tenho ${idade} anos.`)
